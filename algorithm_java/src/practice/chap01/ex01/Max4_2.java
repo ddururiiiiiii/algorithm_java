a5479f9f5e55e4d5ed941b013977618dcd48e2ba
ddururiiiiiii <o244227@daum.net>
2022-07-04 19:04:51 +0900

    01. 알고리즘이란?

---
+++ algorithm_java/src/practice/chap01/ex01/Max4_2.java
@@ -0,0 +1,25 @@
+package practice.chap01.ex01;
+import java.util.Scanner;
+
+class Max4_2 {
+ 
+	public static void main(String[] args) {
+		
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("세 정수의 최댓값을 구합니다.");
+	System.out.print("a의 수 : "); int a = scan.nextInt();
+	System.out.print("b의 수 : "); int b = scan.nextInt();
+	System.out.print("c의 수 : "); int c = scan.nextInt();
+	System.out.print("d의 수 : "); int d = scan.nextInt();
+	
+	int max = a;
+	if (max < b) max = b;
+	if (max < c) max = c;
+	if (max < d) max = d;
+	
+	System.out.println("4개의 수 중에 최대 값은 " + max + " 입니다.");
+	
+	}
+}
+
---
+++ algorithm_java/src/question/chap01/Max3Method.java
@@ -0,0 +1,9 @@
+// 3개의 정숫값 가운데 최댓값을 구하여 출력
+package question.chap01;
+
+class Max3Method {
+	// a, b, c의 최댓값을 구하여 반환
+
+    public static void main(String[] args) {
+    }
+}

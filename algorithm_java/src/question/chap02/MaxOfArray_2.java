d73948c80869b708406e964a332020430f840140
ddururiiiiiii <o244227@daum.net>
2022-07-12 18:23:14 +0900

    3. 배열(3)

---
+++ algorithm_java/src/question/chap02/MaxOfArray_2.java
@@ -0,0 +1,37 @@
+package question.chap02;
+
+
+// 배열 요소의 최댓값 출력(값을 입력받음)
+
+import java.util.Scanner;
+
+class MaxOfArray_2 {
+	// 배열 a의 최댓값을 구하여 반환
+    static int maxOf(int[] a) {
+    	int max = a[0];
+    	for (int i = 1; i < a.length; i++) {
+    		if (a[i] > max)
+    			max = a[i];
+    	}
+    	return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("키의 최댓값을 구합니다.");
+        System.out.print("사람 수: ");
+        int num = scan.nextInt();
+        
+        int height[] = new int[num];
+        
+        for (int i = 0; i < num; i++) {
+        	System.out.print("height : [" + i + "]");
+        	height[i] = scan.nextInt();
+        }
+        
+        System.out.println("최댓값은 " + maxOf(height) +"입니다.");
+        
+        
+    }
+}

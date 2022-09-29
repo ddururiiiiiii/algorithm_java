2917fd847abf555304416ae89f0d19a6e01e1f71
ddururiiiiiii <o244227@daum.net>
2022-09-29 22:21:44 +0900

    인텔리제이 커밋메시지 테스트.

--- algorithm_java/src/programmers/practice01.java
+++ algorithm_java/src/programmers/practice01.java
@@ -14,8 +14,8 @@ public class practice01 {
 	        for (int i = 0; i < 6; i++) {
 	        	System.out.print("내 로또번호 " + (i+1) + "번째 숫자 : ");
 	        	answer[i] = scan.nextInt();
-	        	
-	        	//로또번호 6개 한정
+
+	        	//로또번호 6개 한정 임.
 	        	do {
 	        		System.out.println("로또번호는 1~45번까지만 가능합니다.");
 	        	} while(answer[i] < 1 || answer[i] > 45);
@@ -24,9 +24,9 @@ public class practice01 {
 	        	for (int j = 0; j < answer.length; j++) {
 	        		if (answer[j] == answer[i]) {
 	        		}
-	        		System.out.println("이미 입력된 로또번호입니다.");
-	        		
-	        	}
+					System.out.println("이미 입력된 로또번호입니다.");
+
+				}
 	        }
 	        System.out.println("=== 내 로또번호 입력 끝 ===");
 	        System.out.println(Arrays.toString(answer));
